175b0056b290955ca5ebabc088f1ffb87a390e10
Yang <[scottyang66@gmail.com]>
2018-01-28 13:22:03 -0600

    Add S1 Release cause

--- GUI_Parser.py
+++ GUI_Parser.py
@@ -2,6 +2,7 @@
 Date            Ver No.     Author      History
 01/23/2018      V0.1        Scott Yang  First version
 01/23/2018      V0.2        Scott Yang  Add PM filter,X2HO..
+01/25/2018      V0.3        Scott Yang  Add S1 Release cause
 
 """
 # ! python3.6
@@ -18,7 +19,7 @@ table = {}
 PM = "M8006C268"    #Default PM filter
 
 # Data frame arrangement
-arrange = ["Time", "Cell ID", "UE ID", "CRNTI", "VoLTE", "Error","Failure Phase","S1 Rel Cause","Out Cause", "PM"]
+arrange = ["Time", "Cell ID", "Emil UE ID", "CRNTI","UE Context ID", "VoLTE", "Error","Failure Phase","S1 Rel Cause","Out Cause", "PM"]
 err_list = []
 
 def browse_button():
@@ -34,7 +35,7 @@ def result_button():
 
 def parse_data(data_frame, ErrType, table): #Data parser funcation for different error causes
     for index, row in data_frame.iterrows():
-        table["UE ID"] = row[" Emil UE ID"]
+        table["Emil UE ID"] = row[" Emil UE ID"]
         table["CRNTI"] = row[" CRNTI"]
         table["Cell ID"] = row[" LCR ID"]
         table["VoLTE"] = row[" VoLTE"]
@@ -43,12 +44,15 @@ def parse_data(data_frame, ErrType, table): #Data parser funcation for different
         table["Failure Phase"] = row[" Failure Phase"]
         table["S1 Rel Cause"] = row[" S1 Rel Cause"]
         table["Out Cause"] = row[" Out Cause"]
+        table["UE Context ID"] = row[" UE ID"]
 
         if ErrType == "rlc":
             table["Error"] = row[" Outgoing HO Cause"]
         elif ErrType == "x2fail":
             table["Error"] = row[" Out Cause"]
-        else:
+        elif ErrType == "s1uel":
+            table["Error"] = row[" S1 Rel Cause"]
+        else: #This will take care both "cqirlf" and "puschrlf" bcz both are belong to " RLF Ind List"
             table["Error"] = row[" RLF Ind List"]
         # Need to use dict.copy(),otherwise are just adding references to the same dictionary over and over again:
         err_list.append(table.copy())
@@ -64,9 +68,9 @@ def write_file(data, filetype, write_path): #This funcation is to save the ouptu
         status.insert(END, "Oops! Please make sure the output .xlsx are close..." + '\n')
 
 def emil_parser():  #This is starter funcation after "Run" is clicked
-    PM = pm_value.get()
-    print("Start Parsing")
-    status.insert(END,"Start parsing..." + '\n')
+    PM = pm_value.get() #Reset the PM Filter from GUI input
+    print("Start Parsing....")
+    status.insert(END,"Start parsing...." + '\n')
     current_path = filename + "//*.csv" #file name is getting from the "browse_button" tk funcation
     write_path = filename   #path for output .xlsx files
 
@@ -102,21 +106,29 @@ def emil_parser():  #This is starter funcation after "Run" is clicked
         status.insert(END, "List of X2 HO Failed..." + '\n')
         x2faildf = df[df[' Out Cause'].str.contains(" X2 HO Failed")]
         parse_data(x2faildf, "x2fail", table)
-
+        
+        # Select all call with S1 Release Cause == " S1 Rel Cause"
+        print("List of S1 Rel Casue with Radio Connection with UE Lost...")
+        status.insert(END, "List of S1 Rel Casue with Radio Connection with UE Lost..." + '\n')
+        S1UELdf = df[df[' S1 Rel Cause'].str.contains(" RadioNetworkLayer - Radio Connection With UE Lost")]
+        parse_data(S1UELdf, "s1uel", table)
+        
         # Prepare to output to excel file
         output = pd.DataFrame(err_list)
         write_file(output, "All_Data", write_path)
 
+
     # filter only conatains with the specidfic PM affects KPI
     kpidf = output[output['PM'].str.contains(PM)]
-    write_file(kpidf, "KPI_ONLY", write_path)
+    write_file(kpidf, "PM_Filter_"+PM, write_path)
 
+    
     status.insert(END, "DONE!!!" + '\n')
 
 #### Tk GUI section ######
 window = Tk()
 window.title("Nokia EMIL CSV Parser")
-window.geometry("370x370")
+window.geometry("600x600")
 
 
 #Button for folder locator
@@ -128,14 +140,14 @@ lable=Label(window,text="Load Location:  ")
 lable.grid(row=1,column=0, sticky=W)
 
 #Print folder path
-select_path=Text(window,height=1,width=30)
+select_path=Text(window,height=1,width=50)
 select_path.grid(row=1,column=0, sticky=E)
 
 #Label for PM counter entry
 PM_lable=Label(window,text="PM counter filter:  ")
 PM_lable.grid(row=2,column=0, sticky=W)
 
-#Print folder path
+#Print PM filter
 pm_value=StringVar()
 PM_Filter=Entry(window,textvariable=pm_value)
 PM_Filter.grid(row=2,column=0, sticky=E)
@@ -147,7 +159,7 @@ b1=Button(window,text="Run", command = emil_parser)
 b1.grid(row=3,column=0, sticky=W)
 
 #Status
-status=Text(window,height=15,width=45)
+status=Text(window,height=25,width=70)
 status.grid(row=4,column=0)
 
 #Button for result locator
